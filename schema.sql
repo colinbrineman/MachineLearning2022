09036dc3e37ea5eefda21494905febc0f72b08ef
unknown <alfred.galeno@gmail.com>
2022-05-08 10:51:50 -0400

    final schema upload, aws DB now connected with PG admin data

--- schema.sql
+++ schema.sql
@@ -1,30 +1,29 @@
+DROP TABLE Chicago_Airbnb_Listing_Dataset
+
 -- Creating tables for Airbnb Chicago data
-CREATE TABLE airbnb_data_chicago (
+CREATE TABLE Chicago_Airbnb_Listing_Dataset (
      id INT,
      neighbourhood_cleansed VARCHAR(40) NOT NULL,
-	 latitude INT,
-	 longitude INT,
+	 latitude FLOAT,
+	 longitude FLOAT,
 	 room_type VARCHAR (40) NOT NULL,
 	 accommodates INT,
-	 bathrooms_text INT,
+	 bathrooms_text VARCHAR (40),
 	 bedrooms INT,
 	 beds INT,
-	 price INT,
+	 price MONEY,
 	 minimum_nights INT,
 	 maximum_nights INT,
 	 minimum_minimum_nights INT,
 	 maximum_minimum_nights INT,
 	 minimum_maximum_nights INT,
 	 maximum_maximum_nights INT,
-	 minimum_nights_avg_ntm INT,
-	 maximum_nights_avg_ntm INT,
-	 coordinates NVARCHAR (50),
-	 geopy_address VARCHAR (40),
-	 zip_code INT,
-	 median_household_income INT,
+	 minimum_nights_avg_ntm FLOAT,
+	 maximum_nights_avg_ntm FLOAT,
+	
 	 	
      PRIMARY KEY (id),
      UNIQUE (id)
 );
 
-SELECT * FROM airbnb_data_chicago
+SELECT * FROM Chicago_Airbnb_Listing_Dataset
